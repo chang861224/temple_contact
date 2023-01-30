67740fce264cd3646e4b8dac82b19b05efcf7650
Chi-Hung Chang <chihung816224@gmail.com>
2023-01-30 21:57:51 +0800

    database table in admin page

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from contact import models
 
 # Register your models here.
+admin.site.register(models.PersonUnit)
+admin.site.register(models.PujaUnit)
+admin.site.register(models.DataUnit)
+
